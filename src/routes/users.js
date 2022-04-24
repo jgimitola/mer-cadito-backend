3aea1d9259ab900a3c35c0126ec2db62ade77cef
Jesús Imitola <47477274+jgimitola@users.noreply.github.com>
2022-04-24 16:10:13 -0500

    feat: implement login endpoint

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,6 +8,31 @@ router.get('/', (req, res) => {
   res.json({ message: 'Users' });
 });
 
+router.post('/login', async (req, res, next) => {
+  const body = req.body;
+
+  try {
+    const user = await User.findOne({ username: body.username });
+
+    if (!user._id || user.password !== body.password) return next({ code: 1 });
+
+    res.status(200).json({ _id: user._id });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/prev-login', async (req, res, next) => {
+  const { user_id } = req.body;
+
+  try {
+    const { _id } = await User.findById(user_id);
+    res.status(200).json({ _id });
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.post('/register', async (req, res, next) => {
   const body = req.body;
   const user = new User({
@@ -17,9 +42,8 @@ router.post('/register', async (req, res, next) => {
   });
 
   try {
-    const { _id, display_name, username } = await user.save();
-
-    res.json({ _id, display_name, username });
+    const { _id } = await user.save();
+    res.json({ _id });
   } catch (error) {
     next(error);
   }
