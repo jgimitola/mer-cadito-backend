311c1ea9763bff16490a994492ee9fde8a8b60bc
Jesús Imitola <47477274+jgimitola@users.noreply.github.com>
2022-04-26 20:31:08 -0500

    feat: review routes done

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -4,8 +4,34 @@ import Review from '../models/Review.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Reviews' });
+router.get('/', async (req, res, next) => {
+  const query = req.query;
+
+  const predicate = query.user_id
+    ? { user_id: query.user_id }
+    : { product_id: query.product_id };
+
+  try {
+    const reviews = await Review.find(predicate);
+    res.status(200).json(reviews);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  const body = req.body;
+  try {
+    const review = await Review.create({
+      user_id: body.user_id,
+      product_id: body.product_id,
+      rating: body.rating,
+      description: body.description,
+    });
+    res.status(200).json(review);
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default router;
