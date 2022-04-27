1af2b33fa8e854acb56719df5726b69f6600f11b
Jesús Imitola <47477274+jgimitola@users.noreply.github.com>
2022-04-26 20:07:24 -0500

    feat: cart routes done

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -4,8 +4,53 @@ import Cart from '../models/Cart.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Cart' });
+router.get('/', async (req, res, next) => {
+  const query = req.query;
+  try {
+    const cart = await Cart.find({
+      user_id: query.user_id,
+      sold: false,
+    });
+    res.status(200).json(cart);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  const body = req.body;
+  try {
+    const cart = await Cart.create({
+      product_id: body.product_id,
+      user_id: body.user_id,
+    });
+    res.status(200).json(cart);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/buy', async (req, res, next) => {
+  const body = req.body;
+  try {
+    await Cart.updateMany(
+      { user_id: body.user_id, sold: false },
+      { sold: true }
+    );
+    res.status(200).json({ message: 'Successfully bought' });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/', async (req, res, next) => {
+  const query = req.query;
+  try {
+    const cart = await Cart.findByIdAndDelete(query.item_id);
+    res.status(204).json(cart);
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default router;
